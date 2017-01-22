022d315a8f28cf94b135e080f9ac9d280d75c72a
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2017-01-22 13:37:06 +0300

    update portfolio-dom-na-zolotom-zatone

--- js/portfolio-dom-na-zolotom-zatone.js
+++ js/portfolio-dom-na-zolotom-zatone.js
@@ -1,46 +1,48 @@
-var pswpElement = document.querySelectorAll('.pswp')[0];
-var items = [
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/1.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/2.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/3.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/4.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/5.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/6.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/7.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/8.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/9.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/10.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/11.jpg'
-  },
-  {
-    src: '/images/portfolio/dom-na-zolotom-zatone/12.jpg'
-  }
-];
+$(function() {
+  var pswpElement = document.querySelectorAll('.pswp')[0];
+  var items = [
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/1.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/2.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/3.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/4.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/5.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/6.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/7.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/8.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/9.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/10.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/11.jpg'
+    },
+    {
+      src: '/images/portfolio/dom-na-zolotom-zatone/12.jpg'
+    }
+  ];
 
-var options = {
-    index: 0 // start at first slide
-};
+  var options = {
+      index: 0 // start at first slide
+  };
 
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
+  var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+  gallery.init();
+});
